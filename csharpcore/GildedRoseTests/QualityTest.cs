24ef4b1c9626c68fb2eb4280e7c8329216b799c9
Richard Lewis <lewis.rich@gmail.com>
2024-01-06 21:21:00 +0000

    Added quality tests for backstage passes

--- csharpcore/GildedRoseTests/QualityTest.cs
+++ csharpcore/GildedRoseTests/QualityTest.cs
@@ -123,3 +123,88 @@ public class The_Quality_Of_Aged_Brie
         Assert.That(item.Quality, Is.LessThanOrEqualTo(50), "{0} is more than 50", new[] { nameof(item.Quality) });
     }
 }
+
+public class The_Quality_Of_Backstage_Passes
+{
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 15, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 14, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 13, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 12, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 11, 0)]
+    public void Increases_By_1_When_There_Are_More_Than_10_Days_Before_The_Concert(string name, int sellIn, int quality)
+    {
+        Item item = new() { Name = name, SellIn = sellIn, Quality = quality };
+        var app = new GildedRose(new List<Item> { item });
+
+        app.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(quality + 1), "{0} did not increase by 1. {0} = {1}", new[] { nameof(item.Quality), item.Quality.ToString() });
+    }
+
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 10, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 9, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 8, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 7, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 6, 0)]
+    public void Increases_By_2_When_There_Are_Between_10_Days_And_6_Days_Before_The_Concert(string name, int sellIn, int quality)
+    {
+        Item item = new() { Name = name, SellIn = sellIn, Quality = quality };
+        var app = new GildedRose(new List<Item> { item });
+
+        app.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(quality + 2), "{0} did not increase by 2. {0} = {1}", new[] { nameof(item.Quality), item.Quality.ToString() });
+    }
+
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 5, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 4, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 3, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 2, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 1, 0)]
+    public void Increases_By_3_When_There_Are_5_Days_Or_Less_Before_The_Concert(string name, int sellIn, int quality)
+    {
+        Item item = new() { Name = name, SellIn = sellIn, Quality = quality };
+        var app = new GildedRose(new List<Item> { item });
+
+        app.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(quality + 3), "{0} did not increase by 3. {0} = {1}", new[] { nameof(item.Quality), item.Quality.ToString() });
+    }
+
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 0, 0)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", -1, 20)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", -2, 20)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", -3, 20)]
+    public void Drops_To_0_After_The_Concert(string name, int sellIn, int quality)
+    {
+        Item item = new() { Name = name, SellIn = sellIn, Quality = quality };
+        var app = new GildedRose(new List<Item> { item });
+
+        app.UpdateQuality();
+
+        Assert.That(item.Quality, Is.EqualTo(0), "{0} is not 0", new[] { nameof(item.Quality) });
+    }
+
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 10, 49)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 10, 20)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 5, 7)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 3, 6)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 0, 5)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 3, 48)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 2, 49)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 1, 50)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 0, 48)]
+    public void Is_Never_More_Than_50(string name, int sellIn, int quality)
+    {
+        Item item = new() { Name = name, SellIn = sellIn, Quality = quality };
+        var app = new GildedRose(new List<Item> { item });
+
+        app.UpdateQuality();
+
+        Assert.That(item.Quality, Is.LessThanOrEqualTo(50), "{0} is more than 50", new[] { nameof(item.Quality) });
+    }
+}
