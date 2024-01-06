5869b0f7c2c25e859efa58397fe10521e368227b
Richard Lewis <lewis.rich@gmail.com>
2024-01-06 21:06:58 +0000

    Added quality tests for aged brie

--- csharpcore/GildedRoseTests/QualityTest.cs
+++ csharpcore/GildedRoseTests/QualityTest.cs
@@ -21,7 +21,7 @@ public class The_Quality_Of_A_Normal_Item
         app.UpdateQuality();
 
         Assert.That(item.SellIn, Is.GreaterThanOrEqualTo(0), "{0} date has passed. {0} = {1}", new[] { nameof(item.SellIn), item.SellIn.ToString() });
-        Assert.That(item.Quality, Is.EqualTo(quality-1), "{0} did not decrease by 1. {0} = {1}", new[] { nameof(item.Quality), item.Quality.ToString() });
+        Assert.That(item.Quality, Is.EqualTo(quality - 1), "{0} did not decrease by 1. {0} = {1}", new[] { nameof(item.Quality), item.Quality.ToString() });
     }
 
     [TestCase("normal item", 0, 5)]
@@ -62,6 +62,57 @@ public class The_Quality_Of_A_Normal_Item
     [TestCase("normal item", 2, 49)]
     [TestCase("normal item", 1, 50)]
     [TestCase("normal item", 0, 51)]
+    public void Is_Never_More_Than_50(string name, int sellIn, int quality)
+    {
+        Item item = new() { Name = name, SellIn = sellIn, Quality = quality };
+        var app = new GildedRose(new List<Item> { item });
+
+        app.UpdateQuality();
+
+        Assert.That(item.Quality, Is.LessThanOrEqualTo(50), "{0} is more than 50", new[] { nameof(item.Quality) });
+    }
+}
+
+public class The_Quality_Of_Aged_Brie
+{
+    [TestCase("Aged Brie", 2, 0)]
+    [TestCase("Aged Brie", 3, 5)]
+    [TestCase("Aged Brie", 2, 6)]
+    [TestCase("Aged Brie", 1, 7)]
+    [TestCase("Aged Brie", 1, 1)]
+    public void Increases_By_1_Every_Day_Before_The_Sell_By_Date_Has_Passed(string name, int sellIn, int quality)
+    {
+        Item item = new() { Name = name, SellIn = sellIn, Quality = quality };
+        var app = new GildedRose(new List<Item> { item });
+
+        app.UpdateQuality();
+
+        Assert.That(item.SellIn, Is.GreaterThanOrEqualTo(0), "{0} date has passed. {0} = {1}", new[] { nameof(item.SellIn), item.SellIn.ToString() });
+        Assert.That(item.Quality, Is.EqualTo(quality + 1), "{0} did not increase by 1. {0} = {1}", new[] { nameof(item.Quality), item.Quality.ToString() });
+    }
+
+    [TestCase("Aged Brie", 0, 5)]
+    [TestCase("Aged Brie", -1, 6)]
+    [TestCase("Aged Brie", -2, 7)]
+    public void Increases_Twice_As_Fast_Once_The_Sell_By_Date_Has_Passed(string name, int sellIn, int quality)
+    {
+        Item item = new() { Name = name, SellIn = sellIn, Quality = quality };
+        var app = new GildedRose(new List<Item> { item });
+
+        app.UpdateQuality();
+
+        Assert.That(item.SellIn, Is.LessThan(0), "{0} date has not passed. {0} = {1}", new[] { nameof(item.SellIn), item.SellIn.ToString() });
+        Assert.That(item.Quality, Is.EqualTo(quality + 2), "{0} did not increase by 2. {0} = {1}", new[] { nameof(item.Quality), item.Quality.ToString() });
+    }
+
+    [TestCase("Aged Brie", 10, 20)]
+    [TestCase("Aged Brie", 5, 7)]
+    [TestCase("Aged Brie", 3, 6)]
+    [TestCase("Aged Brie", 0, 5)]
+    [TestCase("Aged Brie", 3, 48)]
+    [TestCase("Aged Brie", 2, 49)]
+    [TestCase("Aged Brie", 1, 50)]
+    [TestCase("Aged Brie", 0, 48)]
     public void Is_Never_More_Than_50(string name, int sellIn, int quality)
     {
         Item item = new() { Name = name, SellIn = sellIn, Quality = quality };
