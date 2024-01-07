4b387476c9808dc8c00e488993cb282788f31a78
Richard Lewis <lewis.rich@gmail.com>
2024-01-07 21:46:46 +0000

    Fixed warnings on ApprovalTest.cs

--- csharpcore/GildedRoseTests/ApprovalTest.cs
+++ csharpcore/GildedRoseTests/ApprovalTest.cs
@@ -1,9 +1,9 @@
-using System;
-using System.IO;
-using System.Text;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using NUnit.Framework;
+using System;
+using System.IO;
+using System.Text;
 
 namespace GildedRoseTests;
 
@@ -17,7 +17,7 @@ public class ApprovalTest
         Console.SetOut(new StringWriter(fakeOutput));
         Console.SetIn(new StringReader($"a{Environment.NewLine}"));
 
-        TextTestFixture.Main(new string[] { "30" });
+        TextTestFixture.Main(["30"]);
         var output = fakeOutput.ToString();
 
         Approvals.Verify(output);
