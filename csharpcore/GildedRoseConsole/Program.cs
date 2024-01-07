c3e407fa72b86ebe8d952858bdf4f99e6c86363e
Richard Lewis <lewis.rich@gmail.com>
2024-01-07 22:54:30 +0000

    Added console app to run from command line

---
+++ csharpcore/GildedRoseConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿using GildedRoseKata;
+
+int? daysOverride = null;
+
+if (args.Length > 0 && int.TryParse(args[0], out int argParsed))
+{
+    daysOverride = argParsed;
+}
+
+Scenario.Run(daysOverride);
