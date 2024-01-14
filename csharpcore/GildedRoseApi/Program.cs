23a2a21e941b776f536b2299ae57d548c4dab1a2
Richard Lewis <lewis.rich@gmail.com>
2024-01-14 15:55:07 +0000

    Added Swagger and Redoc to the API

--- csharpcore/GildedRoseApi/Program.cs
+++ csharpcore/GildedRoseApi/Program.cs
@@ -1,15 +1,43 @@
+using NSwag;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(options =>
+{
+    options.PostProcess = document =>
+    {
+        document.Info = new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Gilded Rose API",
+            Description = "An ASP.NET Core Web API for calling the Gilded Rose functionality."
+        };
+    };
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi();
+
+    // Add ReDoc UI to interact with the document
+    // Available at: http://localhost:<port>/redoc
+    app.UseReDoc(options =>
+    {
+        options.Path = "/redoc";
+    });
 }
 
 app.UseHttpsRedirection();
