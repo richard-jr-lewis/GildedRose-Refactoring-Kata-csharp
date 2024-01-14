91f8042ec507f9f08f759e6396af05773f3cb0d0
Richard Lewis <lewis.rich@gmail.com>
2024-01-14 15:34:49 +0000

    Added API controller

---
+++ csharpcore/GildedRoseApi/Controllers/GildedRoseController.cs
@@ -0,0 +1,18 @@
+﻿using GildedRoseKata;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GildedRoseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GildedRoseController : ControllerBase
+    {
+        // GET: api/<GildedRoseController>
+        [HttpGet]
+        public string Get() => Scenario.Run(null);
+
+        // GET api/<GildedRoseController>/5
+        [HttpGet("{daysOverride}")]
+        public string Get(int daysOverride) => Scenario.Run(daysOverride);
+    }
+}
